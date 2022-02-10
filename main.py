2c1e6ecb055861baf324461a4098959d1d906792
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-02-10 18:42:04 +0800

    Update commands

--- main.py
+++ main.py
@@ -21,10 +21,13 @@ class GlobalState:
     def set_daily_budget(self, budget: float) -> None:
         int_budget = int(budget * 10000)
         self.daily_budget = int_budget
-        self.remaining_today = int_budget
 
     def get_daily_budget(self) -> float:
         return self.daily_budget / 10000
+
+    def set_remaining_today(self, amount: float) -> None:
+        int_amount = int(amount * 10000)
+        self.remaining_today = int_amount
 
     def get_remaining_today(self) -> float:
         return self.remaining_today / 10000
@@ -53,8 +56,9 @@ async def start(message: types.Message):
         await message.answer('''`/hjp`: Vis denne meldingen.
 `/adb 10,00`: Angi dagsbudsjett.
 `/fdb`: Få dagsbudsjett.
-`/brk 3,95`: Bruk et bestemt beløp.
+`/adg 3,52`: Angi dagens gjenstående beløp.
 `/fdg`: Få dagens gjenstående beløp.
+`/brk 3,95`: Bruk et bestemt beløp.
 `/tnd`: Gå til neste dag.
 `/nul`: Tilbakestille.''', parse_mode='Markdown')
 
@@ -64,12 +68,13 @@ async def adb(message: types.Message):
         await message.answer('Feil. Denne boten er designet for kun én bruker.')
     else:
         text = message.get_args()
+        text = text.split(' ', 1)[0]  # use the first argument only
         try:
             assert '.' not in text, 'Should use , for decimal separator instead'
             daily_budget = locale.atof(text)
             gs.set_daily_budget(daily_budget)
             str_daily_budget = locale.format_string('%.4f', gs.get_daily_budget())
-            await message.answer(f'Det nye dagsbudsjettet er {str_daily_budget}.')
+            await message.answer(f'Dagsbudsjettet er satt til {str_daily_budget}.')
         except Exception:
             await message.answer('Feil. Bruk: `/adb 10,00`', parse_mode='Markdown')
 
@@ -83,6 +88,30 @@ async def fdb(message: types.Message):
             await message.answer(f'Dagsbudsjettet er {str_daily_budget}.')
         except Exception as e:
             await message.answer(str(e))
+
+@dp.message_handler(commands=['adg'])
+async def adg(message: types.Message):
+    if message.from_user.id != USER_ID:
+        await message.answer('Feil. Denne boten er designet for kun én bruker.')
+    else:
+        text = message.get_args()
+        text = text.split(' ', 1)[0]  # use the first argument only
+        try:
+            assert '.' not in text, 'Should use , for decimal separator instead'
+            remaining_today = locale.atof(text)
+            gs.set_remaining_today(remaining_today)
+            str_remaining_today = locale.format_string('%.4f', gs.get_remaining_today())
+            await message.answer(f'Dagens gjenstående beløp er satt til {str_remaining_today}.')
+        except Exception:
+            await message.answer('Feil. Bruk: `/adg 3,52`', parse_mode='Markdown')
+
+@dp.message_handler(commands=['fdg'])
+async def fdg(message: types.Message):
+    if message.from_user.id != USER_ID:
+        await message.answer('Feil. Denne boten er designet for kun én bruker.')
+    else:
+        str_remaining_today = locale.format_string('%.4f', gs.get_remaining_today())
+        await message.answer(f'Dagens gjenstående beløp er {str_remaining_today}.')
 
 @dp.message_handler(commands=['brk'])
 async def brk(message: types.Message):
@@ -99,14 +128,6 @@ async def brk(message: types.Message):
             await message.answer(f'Du brukte {str_budget}. Dagens gjenstående beløp er {str_remaining_today}.')
         except Exception:
             await message.answer('Feil. Bruk: `/brk 3,95`', parse_mode='Markdown')
-
-@dp.message_handler(commands=['fdg'])
-async def fdg(message: types.Message):
-    if message.from_user.id != USER_ID:
-        await message.answer('Feil. Denne boten er designet for kun én bruker.')
-    else:
-        str_remaining_today = locale.format_string('%.4f', gs.get_remaining_today())
-        await message.answer(f'Dagens budsjett er {str_remaining_today}.')
 
 @dp.message_handler(commands=['tnd'])
 async def tnd(message: types.Message):
